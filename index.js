04de24c9ec8dc897b0e16f21a65213543d6660e4
brian ambundo <brian.katiechi9@gmail.com>
2022-06-07 16:29:41 +0300

    commit tree d8a8d90114f66ee243a676cd1f6889a5edd10d75

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+  if(feet <= 400){
+    return `This one is on me!`
+  }
+  else if(feet > 2000 && feet < 2500){
+    return `I will gladly take your thirty bucks.`
+  }
+  else if(feet > 2500){
+    return `No can do.`
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
+console.log(scuberGreetingForFeet(300))
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(response){
   // Write your code here!
+return response === 'NYC'? `Ok, sounds good.`: `No go.`
 }
+console.log(ternaryCheckCity("NYC"))
+
+
+function switchOnCharmFromTip(message){
+  // Write your code here!
+  switch(message){
+    case 'generous':
+    return `Thank you so much.`
+    case 'not as generous':
+    return `Thank you.`
+    default:
+      return `Bye.`
+  }
+
+
+}
+console.log(switchOnCharmFromTip('generous'))
